25552bdba331d11a2e4b3af49951ff0ff88f8a93
Gabr1el94 <bielsystem64@gmail.com>
2024-04-14 18:10:05 -0300

    init started project spring with kotlin

---
+++ project-spring-kotlin/src/main/kotlin/com/onegss/hex/app/ProjectSpringKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.onegss.hex.app
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProjectSpringKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<ProjectSpringKotlinApplication>(*args)
+}
---
+++ project-spring-kotlin/src/test/kotlin/com/onegss/hex/app/ProjectSpringKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.onegss.hex.app
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ProjectSpringKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
